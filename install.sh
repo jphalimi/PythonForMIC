8d98d7511ff045e876f90c788c08f3b55e238851
Jean-Philippe Halimi <jean-philippe.halimi@uvsq.fr>
2013-07-05 10:57:59 +0200

    Added an install.sh script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Configuring Python host... "
+./configure
+echo "done";
+
+echo "Compiling Python host... "
+make
+echo "done"
+
+echo "Saving host executables... "
+mv python hostpython
+mv Parser/pgen Parser/hostpgen
+echo "done"
+
+echo "Cleaning build... "
+make distclean
+echo "done"
+
+echo "Applying Xeon Phi patch... "
+patch -p1 < Python-2.6.6-xcompile.patch
+echo "done"
+
+echo "Configuring Python for MIC..."
+./configure CC="icc -mmic"  CXX="icpc -mmic" --host=x86_64 --without-gcc
+echo "done"
+
+echo "Compiling Python for MIC..."
+make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen  CROSS_COMPILE=k1om- CROSS_COMPILE_TARGET=yes HOSTARCH=k1om BUILDARCH=x86_64-linux-gnu EXTRA_CFLAGS="-fp-model precise"
+echo "done"
+
+echo "Installing Python for MIC in ./_binaries/"
+make install HOSTPYTHON=./hostpython  CROSS_COMPILE_TARGET=yes prefix=./_binaries
+echo "done"
+
+echo "You can now copy the content of ./_binaries/ to mic0:/bin"
+echo "Congrats, your installation is ready to use"
+
